78202ef46e28a4c33250a30817d63c48189846b4
Joong Seok Hwang <hjs7949@naver.com>
2024-08-09 11:58:28 +0900

    🎉init : Init project

---
+++ src/main/java/cloud/springdeploymentstudy/SpringDeploymentStudyApplication.java
@@ -0,0 +1,13 @@
+package cloud.springdeploymentstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeploymentStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeploymentStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/springdeploymentstudy/SpringDeploymentStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.springdeploymentstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeploymentStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
