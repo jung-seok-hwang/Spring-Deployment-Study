f8eb0d5e9fc883fab03457da27743a9e76d9990b
Joong Seok Hwang <hjs7949@naver.com>
2024-08-12 15:56:07 +0900

    ✅ test : Add FluxGeneratorService Test Code  for reactive operations
    - We've implemented test code to generate flux from a list, capitalize elements, filter elements based on length, and prove immutability.

---
+++ src/test/java/cloud/springdeploymentstudy/FluxAndMonoGeneratorServiceTest.java
@@ -0,0 +1,80 @@
+package cloud.springdeploymentstudy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+
+class FluxAndMonoGeneratorServiceTest {
+
+    FluxGeneratorService flux = new FluxGeneratorService();
+
+    @Test
+    @DisplayName("Validate Flux fromIterable Methods")
+    public void flux_fromIterable() {
+        //given
+        List<String> list = List.of("A", "B", "C", "D");
+
+        //when
+        var nameFlux = flux.flux(list);
+
+        //then
+        StepVerifier.create(nameFlux)
+                .expectNext("A")
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test Flux Immutability with Map Transformation")
+    public void flux_map() {
+        //given
+        List<String> list = List.of("alex", "job", "hep", "ben");
+
+        //when
+        Flux<String> fluxImmutability = flux.fluxImmutability(list);
+
+        //then
+        StepVerifier.create(fluxImmutability)
+                .expectNext("alex")
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+
+
+    @Test
+    void nameFluxToUpperCase() {
+        //given
+        List<String> list = List.of("alex", "job", "hep", "ben");
+
+        //when
+        var namedFluxToUpperCase = flux.fluxToUpperCase(list);
+
+        //then
+        StepVerifier.create(namedFluxToUpperCase)
+                .expectNext("ALEX","JOB","HEP","BEN")
+                .verifyComplete();
+
+    }
+
+    @Test
+    void fluxFilter() {
+        //given
+        List<String> list = List.of("ale", "job","deadpool", "hep", "ben", "venom");
+
+        //when
+        var namedFluxToUpperCase = flux.fluxFilter(list , 3);
+
+        Flux<String> log = namedFluxToUpperCase.filter(str -> str.length() == 8).log();
+
+        //then
+        StepVerifier.create(log)
+                .expectNext("DEADPOOL")
+                .verifyComplete();
+    }
+
+}
