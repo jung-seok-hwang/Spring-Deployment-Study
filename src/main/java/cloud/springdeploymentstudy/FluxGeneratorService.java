4f0e9aa986d9635f24187c4c7ea8f0bc6b017683
Joong Seok Hwang <hjs7949@naver.com>
2024-08-13 14:45:22 +0900

    ♻️ refactor: Adding synchronous and asynchronous processing Flux features and add-ons
    - Added asynchronous logic to find a user's name that fits a given length.
    - Added operations to find the user's name that meets the criteria using filter, split the name asynchronously into individual characters using the splitNameDelay method, and process each character with a random delay using delayElements.

--- src/main/java/cloud/springdeploymentstudy/FluxGeneratorService.java
+++ src/main/java/cloud/springdeploymentstudy/FluxGeneratorService.java
@@ -3,8 +3,9 @@ package cloud.springdeploymentstudy;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
-
+import java.util.Random;
 
 
 public class FluxGeneratorService {
@@ -28,6 +29,31 @@ public class FluxGeneratorService {
                 .filter(str -> str.length() > length)
                 .map(String::toUpperCase)
                 .log();
+    }
+
+    public Flux<String> fluxFlatMap(List<String> list, int length) {
+        return Flux.fromIterable(list)
+                .filter(str -> str.length() > length)
+                .flatMap(this::splitName)
+                .log();
+    }
+
+    public Flux<String> splitName(String name) {
+        String[] split = name.split("");
+        return Flux.fromArray(split);
+    }
+
+    public Flux<String> fluxFlatMapAsynchronous(List<String> list, int length) {
+        return Flux.fromIterable(list)
+                .filter(str -> str.length() > length)
+                .flatMap(this::splitNameDelay)
+                .log();
+    }
+
+    public Flux<String> splitNameDelay(String name) {
+        String[] split = name.split("");
+        var delay = new Random().nextInt(1000);
+        return Flux.fromArray(split).delayElements(Duration.ofMillis(delay));
     }
 
 }
