01df31771ff5009c0ca9e09b811db48b596832a9
Joong Seok Hwang <hjs7949@naver.com>
2024-08-14 16:56:12 +0900

    ♻️ refactor: Flux 처리 로직 개선 및 비동기 기능 추가
    - Revised method names to be clearer and added logging for each function. nameLength method is still incomplete, and we've also revised the associated test code.

--- src/main/java/cloud/springdeploymentstudy/FluxGeneratorService.java
+++ src/main/java/cloud/springdeploymentstudy/FluxGeneratorService.java
@@ -1,13 +1,15 @@
 package cloud.springdeploymentstudy;
 
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
-
+@Slf4j
 public class FluxGeneratorService {
 
     public Flux<String> flux(List<String> list) {
@@ -34,26 +36,55 @@ public class FluxGeneratorService {
     public Flux<String> fluxFlatMap(List<String> list, int length) {
         return Flux.fromIterable(list)
                 .filter(str -> str.length() > length)
-                .flatMap(this::splitName)
+                .flatMap(this::splitNameIntoCharacters)
                 .log();
-    }
-
-    public Flux<String> splitName(String name) {
-        String[] split = name.split("");
-        return Flux.fromArray(split);
     }
 
     public Flux<String> fluxFlatMapAsynchronous(List<String> list, int length) {
         return Flux.fromIterable(list)
                 .filter(str -> str.length() > length)
-                .flatMap(this::splitNameDelay)
+                .flatMap(str -> nameLength(str , length))
+                .doOnNext(n -> log.info("Done {}", n));
+    }
+
+    public Flux<String> fluxConcatMapAsynchronous(List<String> list, int length) {
+        return Flux.fromIterable(list)
+                .concatMap(str -> nameLength(str , length))
+                .doOnNext(n -> log.info("Done {}", n));
+    }
+
+    public Flux<String> fluxFlatMapSequentialAsynchronous(List<String> list, int length) {
+        return Flux.fromIterable(list)
+                .filter(str -> str.length() > length) //3개를 방출 하고
+                .flatMapSequential(this::splitNameIntoCharactersWithDelay)
                 .log();
     }
 
-    public Flux<String> splitNameDelay(String name) {
+    public Flux<String> splitNameIntoCharacters(String name) {
         String[] split = name.split("");
-        var delay = new Random().nextInt(1000);
-        return Flux.fromArray(split).delayElements(Duration.ofMillis(delay));
+        return Flux.fromArray(split);
     }
 
+    public Flux<String> splitNameIntoCharactersWithDelay(String name) {
+        String[] split = name.split("");
+        var delay = new Random().nextInt(1000);
+
+        return Flux.fromArray(split)
+                .delayElements(Duration.ofMillis(delay))
+                .doOnNext(n -> log.info("Executing {}", n));
+    }
+
+    public Mono<String> nameLength(String name ,Integer length) {
+
+        return name.length() > length ?
+                Mono.just(name).doOnNext(n -> log.info("Executing {}", n)).delayElement(Duration.ofSeconds(1))
+                : Mono.just(name).doOnNext(n -> log.info("Executing {}", n));
+
+    }
+
+//    private Mono<Integer> doSomethingAsync(Integer number) {
+//        //add some delay for the second item...
+//        return number == 2 ? Mono.just(number).doOnNext(n -> log.info("Executing {}", n)).delayElement(Duration.ofSeconds(1))
+//                : Mono.just(number).doOnNext(n -> log.info("Executing {}", n));
+//    }
 }
